bef09d9bd2481c79652b80927160c311a6871379
Elvis Yarquah <elvispk35@hotmail.com>
2026-05-13 11:13:48 +0200

    Solve exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if ((!Number.isInteger(a) || a < 0) || 
+    ((!Number.isInteger(b)) || b < 0)) return "ERROR";
+   
+    let max = 0;
+    let min = 0;
+    if (a < b) {
+        max = b;
+        min = a;
+    } else {
+        max = a;
+        min = b;
+    }
+
+    let total = 0;
+
+    for (let i = min; i <= max; i++) {
+        total += i;
+    }
+    return total;
 
 };
 
