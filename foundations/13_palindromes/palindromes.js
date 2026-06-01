58774c30bbeec62d2ff604c78d2502759befa85e
Elvis Yarquah <elvispk35@hotmail.com>
2026-06-01 13:09:19 +0200

    Write palindromes function and pass tests

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+    const cleaned = sentence
+                            .replace(/[^a-zA-Z0-9]/g, "")
+                            .toLowerCase()
+                            .split("");
+    return cleaned.join("") === cleaned.reverse().join("");  
 
-};
+    };
+
+console.log(palindromes("A car, a man, a maraca"));
 
 // Do not edit below this line
 module.exports = palindromes;
