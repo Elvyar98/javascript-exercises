bdec6e838b85762ee86fa11cd57f004d78fd6f6c
Elvis Yarquah <elvispk35@hotmail.com>
2026-06-02 16:16:19 +0200

    Write function and pass test

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fiboPrev = 1;
+    let fiboPrevPrev = 0;
+    let fibo = 0;
+
+    if (num < 0) return "OOPS";
+    if (num == 0) return 0;
+    if (num == 1) return 1;
+
+    for(let i = 2; i <= num; i++) {
+        fibo = fiboPrev + fiboPrevPrev;
+        fiboPrevPrev = fiboPrev;
+        fiboPrev = fibo
+    };
+
+    return fibo;
 
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
