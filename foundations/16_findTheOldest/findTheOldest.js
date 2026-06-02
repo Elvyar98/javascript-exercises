9950374e3cae9e6dc44ef0114fd54462e30f2515
Elvis Yarquah <elvispk35@hotmail.com>
2026-06-02 16:58:05 +0200

    Write function and pass tests

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+   const getAge = (person) => {
+    const deathYear = person.yearOfDeath || (new Date()).getFullYear();
+    return deathYear - person.yearOfBirth;
+   };
 
+   return arr.reduce((oldest, current) => {
+    console.table(current);
+    console.log(getAge(current));
+    return getAge(current) > getAge(oldest) ? current : oldest;
+   });
 };
 
 // Do not edit below this line
